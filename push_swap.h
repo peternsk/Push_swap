b510ef96b6ea266c4e12dbc13bf863cc9dd0e963
peternsk <petermalonge@gmail.com>
2023-10-20 11:42:50 -0400

    yes

--- push_swap.h
+++ push_swap.h
@@ -6,29 +6,35 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:14:11 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/10/18 12:09:48 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/10/19 15:15:50 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
 
-#include "libft/libft.h"
-#include <string.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <stdbool.h>
-#include <stdio.h>
+# include "libft/libft.h"
+# include <stdbool.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <string.h>
+# include <unistd.h>
 
 //double linked list
 typedef struct s_node
 {
-	struct t_node *prev;
-	int data;
-	struct t_node *next;
-} t_node;
+	struct t_node	*prev;
+	int				data;
+	struct t_node	*next;
+}					t_node;
 
-void	ft_add_node_to_list(char *str);
-void    ft_allow_char(char *str);
+
+typedef struct s_push_utils
+{
+	char			**int_arr;
+}					t_push_utils;
+
+void				ft_add_node_to_list(char *str);
+void				ft_allow_char(char *str);
 
 #endif
--- srcs/utils/ft_add_node_to_list.c
+++ srcs/utils/ft_add_node_to_list.c
@@ -6,11 +6,20 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/17 23:49:22 by peternsaka        #+#    #+#             */
-/*   Updated: 2023/10/18 11:56:15 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/10/19 15:17:13 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../push_swap.h"
+
+/*
+	argv[1] = " 1 2 3 45 65 77 876 4 -374 323 ";
+
+	1) ft_split(argv[1], ' ');
+		==> " "1" "2" "3" "45" "65" "77" "876" "4" "-374" " 323" "
+			creer un array de string que je vais prendre chaque string et le transformer en long int.
+		
+*/
 
 void	ft_add_node_to_list(char *str)
 {
