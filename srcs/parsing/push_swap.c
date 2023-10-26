f7ae4008e15f8fd115cf90461d9c7ba3320ce16d
Peter Nsaka <peternsaka@Peters-MacBook-Pro.local>
2023-10-26 00:22:56 -0400

    push swap

--- srcs/parsing/push_swap.c
+++ srcs/parsing/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: peternsaka <peternsaka@student.42.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:12:32 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/10/19 21:43:20 by peternsaka       ###   ########.fr       */
+/*   Updated: 2023/10/22 19:32:11 by peternsaka       ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,14 @@
 int main(int argc, char **argv)
 {
 	t_push_utils *utils;
+	int	i;
 
+	i = 0;
+	if (argc < 2)
+	{
+		printf("missing or to o many arguments\n");
+		return(EXIT_FAILURE);
+	}
 	if(argc == 2)
 	{
 		ft_allow_char(argv[1]);
@@ -24,8 +31,13 @@ int main(int argc, char **argv)
 	}
 	else
 	{
-		printf("missing or to o many arguments\n");
-		return(EXIT_FAILURE);
+		//technicly this while loop is going to be remplace by an other function
+		while(argv[i])
+		{
+			ft_sign_in_str(argv[i]);
+			printf("%s\n", argv[i]);
+			i++;
+		}
 	}
 	return(EXIT_SUCCESS);
 	
