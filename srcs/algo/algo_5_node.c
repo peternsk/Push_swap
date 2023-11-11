f6bf3965027b455b0afa7fd894d46f2bf0491452
peternsk <petermalonge@gmail.com>
2023-11-11 14:30:08 -0500

    push_swap retour 3

--- srcs/algo/algo_5_node.c
+++ srcs/algo/algo_5_node.c
@@ -6,7 +6,7 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 13:23:50 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/11/11 14:13:02 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/11/11 14:29:18 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,11 +18,13 @@ void    algo_5_node(t_node *stack_a, t_node *stack_b)
     
     printf("max data --> %d\n", min_data(stack_a));
 	printf("max position --> %d\n", min_data_pos(stack_a));
-    
-    if(min_data_pos(stack_a) == 1)
-        printf("push sur la stack\n");
-    if(min_data_pos(stack_a) > 1 && min_data_pos(stack_a) < 4)
-        printf("on monte\n");
-    if(min_data_pos(stack_a) == 4 || min_data_pos(stack_a) == 5)
-        printf("on descend\n");
+    //while(count_node(stack_a) == 3)
+    //{   
+        if(min_data_pos(stack_a) == 1)
+            printf("push sur la stack\n");
+        if(min_data_pos(stack_a) > 1 && min_data_pos(stack_a) < 4)
+            printf("on monte\n");
+        if(min_data_pos(stack_a) == 4 || min_data_pos(stack_a) == 5)
+            printf("on descend\n");
+    //}
 }
