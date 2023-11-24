b36da7ab98d095931f2fd4798db71a5631b3b64c
peternsk <petermalonge@gmail.com>
2023-11-24 16:12:24 -0500

    correction INT_MAX INT_MIN

--- srcs/parsing/parse_str_arg.c
+++ srcs/parsing/parse_str_arg.c
@@ -6,7 +6,7 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/01 12:29:45 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/11/24 15:24:58 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/11/24 16:10:17 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,19 +22,17 @@ bool	parse_str_arg(int arg_n, t_stack *stack, char **arg_v)
 		stack->int_arr = ft_split(arg_v[1], ' ');
 		while (stack->int_arr[i] != NULL)
 		{
-			if((ft_sign_in_str(stack->int_arr[i]) == true && (ft_allow_char(stack->int_arr[i]) == true)))
-				i++;
-			else
+			if((ft_sign_in_str(stack->int_arr[i]) == false && (ft_allow_char(stack->int_arr[i]) == false)))
 				return(false);
+			i++;
 		}
 	}
 	else
 	{
 		while (arg_v[++i] != NULL)
 		{
-			if((ft_allow_char(arg_v[i]) == true) && (ft_sign_in_str(arg_v[i]) == true));
-			else
-				return(false);
+			if((ft_allow_char(arg_v[i]) == false) && (ft_sign_in_str(arg_v[i]) == false))
+				return(false);;
 		}
 	}
 	return(true);
