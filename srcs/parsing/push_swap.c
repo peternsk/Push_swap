9631382c122fcc4d919dd812f79fb3418bb96397
peternsk <petermalonge@gmail.com>
2023-12-05 15:25:22 -0500

    done casse

--- srcs/parsing/push_swap.c
+++ srcs/parsing/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pnsaka <pnsaka@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/08 13:12:32 by pnsaka            #+#    #+#             */
-/*   Updated: 2023/12/05 12:48:21 by pnsaka           ###   ########.fr       */
+/*   Updated: 2023/12/05 15:23:48 by pnsaka           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,21 +20,23 @@ int	main(int argc, char **argv)
 	{
 		stack = create_push_utils();
 		if (parse_str_arg(argc, stack, argv) == false)
-			return (ft_putstr_fd("Error parse..", 2), free(stack),
-				EXIT_FAILURE);
-		if (tap_str_to_tab_int(stack, argc, argv) == false)
-			return (ft_putstr_fd("Error int", 2),
+			return (ft_putendl_fd("Error", 2), free_2d_arr(stack->int_arr),
 				free(stack), EXIT_FAILURE);
+		if (tap_str_to_tab_int(stack, argc, argv) == false)
+			return (ft_putendl_fd("Error", 2),
+					free(stack),
+					EXIT_FAILURE);
 		stack->a = init_stack(argc, stack);
 		if (check_dup_data(stack->a) == true)
-			return (0);
-		if (is_stack_sorted(stack->a) == false)
 		{
-			algos(stack);
-			free_stack(&stack->a);
-			free(stack->val_tab);
-			free(stack);
+			ft_putendl_fd("Error", 2);
+			return (0);
 		}
+		if (is_stack_sorted(stack->a) == false)
+			algos(stack);
+		free_stack(&stack->a);
+		free(stack->val_tab);
+		free(stack);
 	}
 	else
 		return (EXIT_FAILURE);
